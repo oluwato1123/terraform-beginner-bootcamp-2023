b636f8f8a9ddb27cd5bc0adf7e698dafcdbfc204
Tobi Owofade <oowofade@gmail.com>
2023-09-30 17:23:52 +0000

    wip

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 terraform {
-  #   cloud {
-  #     organization = "oluwato-terraform"
+    cloud {
+      organization = "oluwato-terraform"
 
-  #     workspaces {
-  #       name = "terra-house-1"
-  #     }
-  #   }
+      workspaces {
+        name = "terraform-cloud"
+      }
+    }
 
 
 }
